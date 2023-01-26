83838a030e3547b3854baba4f9b4bbcfdcf40e45
keyboard <123636359+Mohamed-Taher1@users.noreply.github.com>
2023-01-26 18:56:04 +0000

    new code

--- .ccls-cache/@home@runner@Code/main.c
+++ .ccls-cache/@home@runner@Code/main.c
@@ -3,12 +3,12 @@
 
 int main() {
 
-  char username[100],domain[100];
+  char username[100], domain[100];
   printf("Email: ");
-  scanf("%[^@]",username);
-  scanf("%[^\n]",domain);
-  printf("%s\n",username);
-  printf("%s",domain);
+  scanf("%[^@]", username);
+  scanf("%[^\n]", domain);
+  printf("%s\n", username);
+  printf("%s", domain);
 
   return 0;
 }
---
+++ generateWebPage/code.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+char x[] =
+
+    int main() {}
